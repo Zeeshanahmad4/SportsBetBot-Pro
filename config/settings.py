6f9d29a38ef4fbb4732220c208fd238dae7e2e49
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-07 12:58:56 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,20 @@
-# config/settings.py 
- 
-TELEGRAM_API_KEY = "YOUR_TELEGRAM_API_KEY" 
-SPORTSBOOK_API_KEY = "YOUR_SPORTSBOOK_API_KEY" 
-TELEGRAM_GROUP_ID = "YOUR_TELEGRAM_GROUP_ID" 
-SPORTSBOOK_URL = "https://sportsbook.example.com" 
+# config/settings.py
+
+# Telegram configuration
+TELEGRAM_API_KEY = "YOUR_TELEGRAM_API_KEY"
+TELEGRAM_GROUP_ID = "YOUR_TELEGRAM_GROUP_ID"
+
+# Sportsbook configuration
+SPORTSBOOK_API_KEY = "YOUR_SPORTSBOOK_API_KEY"
+SPORTSBOOK_URL = "https://sportsbook.example.com"
+
+# Odds criteria (example: a threshold for minimum acceptable odds)
+MINIMUM_ODDS = 1.5
+
+# Bet amount (example: default amount to place on each bet)
+DEFAULT_BET_AMOUNT = 10.0
+
+# Stealth mode settings (example: a delay to mimic human activity)
+STEALTH_DELAY_SECONDS = 5
+
+# Other global settings can be added here
