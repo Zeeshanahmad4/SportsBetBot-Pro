424a05e3a05c942275d230f85220c2c5e5372101
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-07 13:04:49 +0500

    Update odds_verifier.py

--- sportsbook/odds_verifier.py
+++ sportsbook/odds_verifier.py
@@ -1,4 +1,9 @@
-# sportsbook/odds_verifier.py 
- 
-def verify_odds(odds): 
-   pass 
+# sportsbook/odds_verifier.py
+from config import settings
+
+def verify_odds(odds):
+    # Compare the provided odds with the set criteria from settings
+    if odds >= settings.MINIMUM_ODDS:
+        return True  # The odds meet the criteria
+    else:
+        return False
