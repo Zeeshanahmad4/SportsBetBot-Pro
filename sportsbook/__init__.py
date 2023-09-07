890fda8a55c7d9b8b773702f70917afeac4ec964
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-07 13:07:51 +0500

    Update __init__.py

--- sportsbook/__init__.py
+++ sportsbook/__init__.py
@@ -1,1 +1,8 @@
- 
+# sportsbook/__init__.py
+
+"""
+This package contains modules for interfacing with the sportsbook.
+- search: Searches for the bet on the sportsbook.
+- odds_verifier: Verifies if the odds meet the set criteria.
+- placer: Places the bet on the sportsbook.
+""" 
