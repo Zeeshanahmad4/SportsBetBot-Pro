969cc26e896570e88414b896ddda4112e12fc5d7
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-07 13:04:21 +0500

    Update search.py

--- sportsbook/search.py
+++ sportsbook/search.py
@@ -1,5 +1,13 @@
-# sportsbook/search.py 
-from config import settings 
- 
-def search_bet(bet_details): 
-   pass 
+# sportsbook/search.py
+from config import settings
+
+def search_bet(bet_details):
+    # Connect to SPORTSBOOK_URL using SPORTSBOOK_API_KEY
+    # Search for the bet using bet_details
+    
+    found_bet = None  # Placeholder. This would be the result of the search.
+    
+    if found_bet:
+        return found_bet  # Return the bet details from the sportsbook
+    else:
+        return None
