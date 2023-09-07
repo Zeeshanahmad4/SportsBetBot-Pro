2a417c561eb43b9a057c5d387c9b8686cfd49e53
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-07 13:14:04 +0500

    Update stealth.py

--- utilities/stealth.py
+++ utilities/stealth.py
@@ -1,4 +1,14 @@
-# utilities/stealth.py 
- 
-def disguise_bot_activity(): 
-   pass 
+# utilities/stealth.py
+import random
+import time
+
+def disguise_bot_activity():
+    """
+    Introduce random delays and other stealth techniques 
+    to make the bot's actions appear more human-like.
+    """
+    # Introduce a random delay between 1 to 5 seconds 
+    # to mimic human-like intervals between actions.
+    time.sleep(random.uniform(1, 5))
+    
+    # Further stealth techniques can be added here.
