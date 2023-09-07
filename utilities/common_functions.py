ca8d5846e7be18b7e8bb64a9ef8b7c7121332329
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-07 13:14:39 +0500

    Update common_functions.py

--- utilities/common_functions.py
+++ utilities/common_functions.py
@@ -1,4 +1,9 @@
-# utilities/common_functions.py 
- 
-def some_common_function(): 
-   pass 
+# utilities/common_functions.py
+
+def format_bet_details(bet_details: str) -> str:
+    """
+    Format the bet details in a standardized way.
+    For demonstration purposes, this function will just 
+    return the bet details in uppercase.
+    """
+    return bet_details.upper()
