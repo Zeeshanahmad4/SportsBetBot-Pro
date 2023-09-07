e135160145b5b3c7d39971872ed9a59d4a87cb3d
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-07 13:11:18 +0500

    Update __init__.py

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -1,1 +1,7 @@
- 
+# utilities/__init__.py
+
+"""
+This package contains utility modules for the bot.
+- stealth: Functions to make the bot operate discreetly.
+- common_functions: General utility functions used across the bot.
+""" 
